93674d6ad3cefc0dd866698720f5663112271727
jonathanhansson <jonathanhanssondg@gmail.com>
2025-04-29 13:33:36 +0200

    FEAT: Transformed the dimensional table for employers to ensure data quality.

--- ai_dbt_project/models/refined/dim/dim_employer.sql
+++ ai_dbt_project/models/refined/dim/dim_employer.sql
@@ -16,12 +16,12 @@ SELECT
         'employer_org_nr', 
         'street_address'
     ])}} AS employer_id,
-    employer_name,
-    workplace,
-    employer_org_nr,
-    street_address,
-    postal_code,
-    region,
-    city,
+    LOWER(COALESCE(employer_name, 'ej angiven')) as employer_name,
+    LOWER(COALESCE(workplace, 'ej angiven')) as workplace,
+    LOWER(COALESCE(employer_org_nr, 'ej angiven')) as employer_org_nr,
+    LOWER(COALESCE(street_address, 'ej angiven')) as street_address,
+    LOWER(COALESCE(postal_code, 'ej angiven')) as postal_code,     
+    LOWER(COALESCE(region, 'ej angiven')) as region,
+    LOWER(COALESCE(city, 'ej angiven')) as city,
     country
 FROM base
