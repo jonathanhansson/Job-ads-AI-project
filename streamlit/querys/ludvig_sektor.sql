1ae05664830ef51ad0ab7285a1cb1d55f47208e8
jonathanhansson <jonathanhanssondg@gmail.com>
2025-05-02 17:11:11 +0200

    FEAT: Separated all Ludvigs querys into separate SQL-files to further modulize the project

---
+++ streamlit/querys/ludvig_sektor.sql
@@ -0,0 +1,10 @@
+    SELECT
+        oc.occupation_field AS TargetGroup,
+        SUM(fj.number_vacancies) AS Vacancies,
+        MIN(oc.occupation_field) AS Industry
+    FROM refined.fct_jobs AS fj
+    JOIN refined.dim_occupation oc ON fj.occupation_id = oc.occupation_id
+    WHERE fj.publication_date BETWEEN '{start_date}' AND '{end_date}'
+    GROUP BY TargetGroup
+    ORDER BY Vacancies DESC
+    LIMIT 5
