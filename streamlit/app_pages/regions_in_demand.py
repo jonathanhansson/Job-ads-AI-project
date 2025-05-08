59dee60c67c02951cb3c9f9564e769447f7f793d
jonathanhansson <jonathanhanssondg@gmail.com>
2025-05-08 15:47:51 +0200

    FEAT: Python/Streamlit logic for open positions per region

---
+++ streamlit/app_pages/regions_in_demand.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from st_db_con import get_connection, render_sql, run_query
+import plotly.express as px
+from datetime import date
+
+def get_regions():
+    order_style = ["ASC", "DESC"]
+
+    col1, col2 = st.columns(2)
+
+    
+    with col1:
+        sorting_choice = st.radio(
+            "Välj sortering:",
+            ["Stigande", "Fallande"]
+        )
+
+        date_start = date(2025, 4, 25)
+        date_end = date.today()
+
+        start_date, end_date = st.date_input(
+            "Välj datumintervall: ",
+            value=(date_start, date_end),
+            min_value=date(2025, 4, 25),
+            max_value=date.today()
+        )
+
+        with get_connection() as con:
+            if sorting_choice == "Stigande":
+                query = render_sql("region", order_style=order_style[0])
+            else:
+                query = render_sql("region", order_style=order_style[1])
+
+            df = run_query(con, query, params=[start_date, end_date])
+        
+        # filtrerar bort "ej angiven"
+        df = df[df["region"] != "ej angiven"]
+
+    with col2:
+        st.button("INFO", help="I vår databas finns det jobb som inte har någon region specificerad.")
+        st.subheader("Svenska regioner med flest jobbmöjligheter")
+        fig = px.bar(
+            df,
+            x = "region",
+            y = "open_jobs"
+        )
+        
+        fig.update_layout(xaxis_tickangle=290)
+        
+
+        st.plotly_chart(fig, use_container_width=True)
+        
+    
